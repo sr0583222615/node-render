3d86a479883d2726385a4ffe76b13c60134262bb
Shifi <s3222615@gmail.com>
2025-01-14 20:07:23 +0200

    server

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const express = require('express');
 const axios = require('axios');
-
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // Route to get list of Render services
 app.get('/services', async (req, res) => {
-  const apiKey = process.env.RENDER_API_KEY; // API Key of Render
+  const apiKey = rnd_JRRLJWOjjHOQ9uWviLpOtqFC0L7s; // API Key of Render
   const url = 'https://api.render.com/v1/services?limit=20';
   
   try {
     const response = await axios.get(url, {
       headers: {
-        'Authorization': `Bearer ${rnd_JRRLJWOjjHOQ9uWviLpOtqFC0L7s}`
+        'Authorization': `Bearer ${apiKey}` // השתמש במפתח הסביבה
       }
     });
     res.json(response.data);
@@ -22,8 +21,8 @@ app.get('/services', async (req, res) => {
   }
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
-});
 
+
+});
